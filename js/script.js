d3e30e539fdf8a574319e9fbdf606c2895e8beba
mgarbacz <mich.garbacz@gmail.com>
2012-10-31 13:59:42 -0500

    Added on hover event to show full text of where links go

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('#a-gh').hover(
+  function() {
+    $(this).html('github');
+  }, function() {
+    $(this).html('gh');
+});
+$('#a-so').hover(
+  function() { 
+    $(this).html('stackoverflow'); 
+  }, function() { 
+    $(this).html('so');
+});
