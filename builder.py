bbb0eb21f084f8d1fcc65665b80bb12c798b37fb
mgarbacz <mich.garbacz@gmail.com>
2012-11-19 19:49:19 -0600

    Reformatted python builder script a bit. Still more to do

--- builder.py
+++ builder.py
@@ -1,4 +1,4 @@
-import boto, os, sys, time
+import boto, os, time
 
 def print_result(subject, action, success):
   modifier = ' not '
@@ -18,21 +18,15 @@ def build_stylesheets():
 
   return print_result('Stylesheets', 'built', success)
 
-def file_has_been_modified(local_date, s3_date):
-  # Is the local modified date newer than s3 modified date?
-  if (local_date > s3_date):
-    return True
-  return False
-
-def print_upload_progress(complete, total):
-  sys.stdout.write('.')
-  sys.stdout.flush()
+def format_s3_date(s3_date):
+  s3_time_format = '%a, %d %b %Y %H:%M:%S %Z'
+  return time.mktime(time.strptime(s3_date, s3_time_format))
 
 def commit_to_s3():
   success = False
 
-  # Requires S3 creds, which I set as environment variables
   try:
+    # Requires S3 creds, which I set as environment variables
     connection = boto.connect_s3();
     bucket = connection.lookup('www.michgarbacz.com')
 
@@ -42,18 +36,20 @@ def commit_to_s3():
         # For s3, we don't want the 'web-content' part of file path
         s3_file_path = local_file_path[12:]
 
-        print filename
+        print filename,
         local_last_modified = os.path.getmtime(local_file_path)
-        s3_last_modified = bucket.get_key(s3_file_path).last_modified
-        s3_time_format = '%a, %d %b %Y %H:%M:%S %Z'
-        s3_last_modified = time.mktime(time.strptime(s3_last_modified, s3_time_format))
+        s3_last_modified = format_s3_date(bucket
+          .get_key(s3_file_path).last_modified)
 
-        if (file_has_been_modified(local_last_modified, s3_last_modified)):
+        # Is the local modified date newer than s3 modified date?
+        if (local_last_modified > s3_last_modified):
           key_file = boto.s3.key.Key(bucket)
           key_file.key = s3_file_path
-          key_file.set_contents_from_filename(local_file_path,
-            cb=print_upload_progress, num_cb=10)
+          print 'is updating...'
+          key_file.set_contents_from_filename(local_file_path)
           key_file.make_public()
+        else:
+          print 'is up to date.'
 
     success = True
 
