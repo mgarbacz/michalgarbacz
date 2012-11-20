82084ca1b20056a6521a2ddf7f6ee6a3cde79185
mgarbacz <mich.garbacz@gmail.com>
2012-11-20 15:50:34 -0600

    Minor formatting

--- builder.py
+++ builder.py
@@ -3,4 +3,5 @@ from builder import javascript_builder, stylesheet_builder, s3_committer
 javascript_builder.build_javascripts()
 stylesheet_builder.build_stylesheets()
 s3_committer.commit_to_s3()
+
 print 'Task done.'
