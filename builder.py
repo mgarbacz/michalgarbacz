b0ad4e9b6fe39cddddd8c72e81e8977ae5cbacb6
mgarbacz <mich.garbacz@gmail.com>
2012-11-19 18:59:44 -0600

    Updated python builder script to check last modified dates to only upload modified files. Code needs cleaning and clarifying

--- builder.py
+++ builder.py
@@ -1,4 +1,4 @@
-import boto, os
+import boto, os, sys, time
 
 def print_result(subject, action, success):
   modifier = ' not '
@@ -18,6 +18,16 @@ def build_stylesheets():
 
   return print_result('Stylesheets', 'built', success)
 
+def file_has_been_modified(local_date, s3_date):
+  # Is the local modified date newer than s3 modified date?
+  if (local_date > s3_date):
+    return True
+  return False
+
+def print_upload_progress(complete, total):
+  sys.stdout.write('.')
+  sys.stdout.flush()
+
 def commit_to_s3():
   success = False
 
@@ -32,10 +42,18 @@ def commit_to_s3():
         # For s3, we don't want the 'web-content' part of file path
         s3_file_path = local_file_path[12:]
 
-        key_file = boto.s3.key.Key(bucket)
-        key_file.key = s3_file_path
-        key_file.set_contents_from_filename(local_file_path)
-        key_file.make_public()
+        print filename
+        local_last_modified = os.path.getmtime(local_file_path)
+        s3_last_modified = bucket.get_key(s3_file_path).last_modified
+        s3_time_format = '%a, %d %b %Y %H:%M:%S %Z'
+        s3_last_modified = time.mktime(time.strptime(s3_last_modified, s3_time_format))
+
+        if (file_has_been_modified(local_last_modified, s3_last_modified)):
+          key_file = boto.s3.key.Key(bucket)
+          key_file.key = s3_file_path
+          key_file.set_contents_from_filename(local_file_path,
+            cb=print_upload_progress, num_cb=10)
+          key_file.make_public()
 
     success = True
 
