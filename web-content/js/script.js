03937b19d7e003e8bcf733d3259d9a38dd71ad6e
mgarbacz <mich.garbacz@gmail.com>
2012-12-13 17:20:49 -0600

    Added cprogress for skill display

--- web-content/js/script.js
+++ web-content/js/script.js
@@ -12,3 +12,20 @@ $('#a-stackoverflow').hover(
   }, function() { 
     $(this).html('so');
 });
+
+var myplugin;
+if(!myplugin){
+  myplugin = $('#skill-python').cprogress({
+    percent: 10, // starting position
+    img1: 'img/v1.png', // background
+    img2: 'img/v2.png', // foreground
+    speed: 200, // speed (timeout)
+    PIStep : 0.05, // every step foreground area is bigger about this val
+    limit: 20, // end value
+    loop : false, //if true, no matter if limit is set, progressbar will be running
+    showPercent : true, //show hide percent
+    onInit: function(){console.log('onInit');},
+    onProgress: function(p){console.log('onProgress',p);}, //p=current percent
+    onComplete: function(p){console.log('onComplete',p);}
+  });
+}
