d19d64c23a214ef610c5d60314483441e27dc803
mgarbacz <mich.garbacz@gmail.com>
2012-12-21 15:46:39 -0600

    Generalized skill canvas creation a bit, working on styling and the fill bar look

--- web-content/js/script.js
+++ web-content/js/script.js
@@ -13,29 +13,37 @@ $('#a-stackoverflow').hover(
     $(this).html('so');
 });
 
+var img1 = new Image();
+img1.src = 'img/v1.png';
+var img2 = new Image();
+img2.src = 'img/v2.png';
+
+function drawSkillCanvas(skillLevel) {
+  var skillCanvas= $('<canvas />');
+  var context = skillCanvas[0].getContext("2d");
+  var i = skillLevel*(Math.PI*2)/100;
+  var j = 0;
+  context.fillStyle = "rgba(0,0,0,0.0)";
+  context.clearRect(0,0,img1.width,img1.height);
+  context.save();
+  context.drawImage(img1, 0, 0);
+  context.beginPath();
+  context.lineWidth = 5;
+  context.arc(img1.width/2,img1.height/2,img1.height/2,i-Math.PI/2,j-Math.PI/2,true);
+  context.lineTo(img1.width/2,img1.height/2);
+  context.closePath();
+  context.fill();
+  context.clip();
+  context.drawImage(img2,0,0);
+  context.restore();
+
+  return skillCanvas;
+}
+
 window.onload = init;
 function init() {
-  var img1 = new Image();
-  img1.src = 'img/v1.png';
-  var img2 = new Image();
-  img2.src = 'img/v2.png';
-  var canvas_ruby = $('<canvas />');
-  var ctx = canvas_ruby[0].getContext("2d");
-  var i = 66*(Math.PI*2)/100;
-  var j = 0;
-    ctx.fillStyle = "rgba(0,0,0,0.0)";
-    ctx.clearRect(0,0,img1.width,img1.height);
-    ctx.save();
-    ctx.drawImage(img1, 0, 0);
-      ctx.beginPath();
-      ctx.lineWidth = 5;
-      ctx.arc(img1.width/2,img1.height/2,img1.height/2,i-Math.PI/2,j-Math.PI/2,true);
-      ctx.lineTo(img1.width/2,img1.height/2);
-      ctx.closePath();
-      ctx.fill();
-      ctx.clip();
-      ctx.drawImage(img2,0,0);
-    ctx.restore();
-
-  $('#skill-ruby').append(canvas_ruby);
+  $('#skill-ruby').append(drawSkillCanvas(70));
+  $('#skill-python').append(drawSkillCanvas(65));
+  $('#skill-javascript').append(drawSkillCanvas(95));
+  $('#skill-erlang').append(drawSkillCanvas(40));
 }
