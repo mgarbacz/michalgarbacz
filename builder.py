afe7b0372cb22e2ce9f491c5491fe8f8fd61a4d5
mgarbacz <mich.garbacz@gmail.com>
2013-02-21 22:57:48 -0600

    Getting rid of javascript and stylesheet builder

--- builder.py
+++ builder.py
@@ -1,7 +1,5 @@
-from builder import javascript_builder, stylesheet_builder, s3_committer
+from builder import s3_committer
 
-javascript_builder.build_javascripts()
-stylesheet_builder.build_stylesheets()
 s3_committer.commit_to_s3()
 
 print 'Task done.'
