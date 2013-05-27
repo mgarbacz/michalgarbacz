ee8aa284786c1b6d699c0c5d8d003c38b8c3b2cf
mgarbacz <mich.garbacz@gmail.com>
2013-05-27 09:20:37 -0500

    Getting rid of the time-based modifications only upload. Build process modifies the file each time, so we have no need for the check, everything will have been modified. Mayhaps do something better?

--- builder/s3_committer.py
+++ builder/s3_committer.py
@@ -1,14 +1,6 @@
 import boto, os, time
 from builder import results_printer
 
-# Boto provides s3 date in string format, we need seconds since the epoch
-def format_s3_date(s3_date):
-  # S3 string format is 'Sun, 01 Jan 2012 00:00:00 GMT'
-  s3_time_format = '%a, %d %b %Y %H:%M:%S %Z'
-  # Using the s3 given date and its format, strptime gives us a struct_time
-  # which is made into time (seconds since the epoch) using mktime
-  return time.mktime(time.strptime(s3_date, s3_time_format))
-
 # We want to commit all modified files to the s3 bucket for the site
 def commit_to_s3():
   success = False
@@ -26,22 +18,13 @@ def commit_to_s3():
         # For s3, we don't want the 'public' part of file path
         s3_file_path = local_file_path[7:]
 
-        # We need the times for local and s3 last modified
-        local_last_modified = os.path.getmtime(local_file_path)
         remote_file = bucket.get_key(s3_file_path)
-        # If the file is new, it won't exist in bucket so we want to add it
-        if remote_file is None:
-          s3_last_modified = 0
-        else:
-          s3_last_modified = format_s3_date(remote_file.last_modified)
 
-        # Has the file been modified since it was uploaded?
-        if (local_last_modified > s3_last_modified):
-          print filename + ' is uploading...'
-          key_file = boto.s3.key.Key(bucket)
-          key_file.key = s3_file_path
-          key_file.set_contents_from_filename(local_file_path)
-          key_file.make_public()
+        print filename + ' is uploading...'
+        key_file = boto.s3.key.Key(bucket)
+        key_file.key = s3_file_path
+        key_file.set_contents_from_filename(local_file_path)
+        key_file.make_public()
         
         # Will print after update or if no update was required
         print filename + ' is up to date.'
