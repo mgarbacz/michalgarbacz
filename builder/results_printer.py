3d304b2f6a3f60a626df7d8032c28e38e2675ffb
Michal Garbacz <mich.garbacz@gmail.com>
2013-09-30 00:51:30 -0500

    Delete results_printer.py
    Removing builder since Bucketeer is ready for generic use.

--- builder/results_printer.py
+++
@@ -1,6 +0,0 @@
-# We are doing 3 separate tasks here, but we want a standard report
-def print_result(subject, action, success):
-  modifier = ' not '
-  if success == True:
-    modifier = ' '
-  print subject + modifier + action + '.'
